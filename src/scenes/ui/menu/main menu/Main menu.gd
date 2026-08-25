a108a0b7f5194852f73c62c690cee10fdc4a35f1
DarlysonGabriel <darlysongabriel.contato@gmail.com>
2026-08-25 11:11:16 -0300

    update: atualizei o menu e os bg's

--- src/scenes/ui/menu/main menu/Main menu.gd
+++ src/scenes/ui/menu/main menu/Main menu.gd
@@ -9,7 +9,12 @@ func _ready():
 	animation.play("Camera_IN")
 
 func _on_play_pressed():
+	animation.play("Play_out")
+	await animation.animation_finished
 	get_tree().change_scene_to_file("res://src/scenes/levels/Level tutorial/Level Tuto.tscn")
 
-func _on_quit_2_pressed() -> void:
+func _on_restart_pressed():
 	get_tree().change_scene_to_file("res://src/scenes/ui/menu/main menu/Main menu.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit(0)
